4a3cef73a17bf0d8ad8c98f8d76cb49e35c62b2c
randallwhitlock <randall.whitlock@live.com>
2018-02-28 18:48:37 -0500

    recursion.js added

---
+++ exercises/recursion.js
@@ -0,0 +1,49 @@
+//complete the following using recursion
+
+// 1. Compute the sum of an array of integers.
+// sum([1,2,3,4,5,6]); // 21
+var sum = function(array) {
+};
+
+// 2. Sum all numbers in an array containing nested arrays.
+// arraySum([1,[2,3],[[4]],5]); // 15
+var arraySum = function(array) {
+};
+
+// 3. Check if a number is even.
+var isEven = function(n) {
+};
+
+// 4. Sum all integers below a given integer.
+// sumBelow(10); // 45
+// sumBelow(7); // 21
+var sumBelow = function(n) {
+};
+
+// 5 Get the integers within a range (x, y).
+// range(2,9); // [3,4,5,6,7,8]
+var range = function(x, y) {
+};
+
+// 6. Compute the exponent of a number.
+// The exponent of a number says how many times the base number is used as a factor.
+// 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
+// exponent(4,3); // 64
+// https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
+var exponent = function(base, exp) {
+};
+
+// 7. Determine if a number is a power of two.
+// powerOfTwo(1); // true
+// powerOfTwo(16); // true
+// powerOfTwo(10); // false
+var powerOfTwo = function(n) {
+};
+
+// 8. Write a function that reverses a string.
+var reverse = function(string) {
+};
+
+// 9. Write a function that determines if a string is a palindrome.
+var palindrome = function(string) {
+};
